387cec9c2cdde6bdd68b4a32c6204e4c34259632
jeff0518 <cycivs07@icloud.com>
2022-10-08 16:32:14 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//mongoose 連線
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true,  useUnifiedTopology: true } ) // 設定連線到 mongoDB
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+
 app.get('/', (req, res) => {
   res.send('Hello Jeff')
 })
 
 app.listen(port, (req, res) => {
-  console.log(`App is running on localhost:${port}`)
+  console.log(`App is running on http://localhost:${port}`)
 })
