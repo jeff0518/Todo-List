7dfdc556bf680638ced4c03c71415c0c6a8dc2c8
jeff0518 <cycivs07@icloud.com>
2022-10-09 18:52:01 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -69,6 +69,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, (req, res) => {
   console.log(`App is running on http://localhost:${port}`)
 })
