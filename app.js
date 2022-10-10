c46f1db2f1ed96399d7c95e90caff84704b0294d
jeff0518 <cycivs07@icloud.com>
2022-10-11 00:58:40 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,84 +1,91 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const Todo = require('./models/todo')
+const express = require("express");
+const app = express();
+const port = 3000;
+// 載入 method-override
+const methodOverride = require('method-override') 
+
+const Todo = require("./models/todo");
 //Handlebars 設定
-const exphbs = require('express-handlebars')
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+const exphbs = require("express-handlebars");
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 //mongoose 連線
-const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true,  useUnifiedTopology: true } ) // 設定連線到 mongoDB
-const db = mongoose.connection
+const mongoose = require("mongoose"); // 載入 mongoose
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); // 設定連線到 mongoDB
+const db = mongoose.connection;
 // 引用 body-parser
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-app.use(bodyParser.urlencoded({ extended: true }))
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride('_method'))
+db.on("error", () => {
+  console.log("mongodb error!");
+});
 
-db.once('open', () => {
-  console.log('mongodb connected')
-})
+db.once("open", () => {
+  console.log("mongodb connected");
+});
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   Todo.find()
     .lean()
     .sort({ _id: "asc" })
     .then((todos) => res.render("index", { todos }))
     .catch((error) => console.error(error));
-})
+});
 
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
+app.get("/todos/new", (req, res) => {
+  return res.render("new");
+});
 
-app.post('/todos', (req, res) => {
-  const name =req.body.name
+app.post("/todos", (req, res) => {
+  const name = req.body.name;
   return Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
+});
 
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
   return Todo.findById(id)
     .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
+    .then((todo) => res.render("detail", { todo }))
+    .catch((error) => console.log(error));
+});
 
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
+app.get("/todos/:id/edit", (req, res) => {
+  const id = req.params.id;
   return Todo.findById(id)
     .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
+    .then((todo) => res.render("edit", { todo }))
+    .catch((error) => console.log(error));
+});
 
-app.post('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  const {name, isDone } = req.body
+app.put("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, isDone } = req.body;
   return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
+    .then((todo) => {
+      todo.name = name;
+      todo.isDone = isDone === "on";
+      return todo.save();
     })
-    .then(()=> res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch((error) => console.log(error));
+});
 
-app.post('/todos/:id/delete', (req, res) => {
-  const id = req.params.id
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
   return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+    .then((todo) => todo.remove())
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
+});
 
 app.listen(port, (req, res) => {
-  console.log(`App is running on http://localhost:${port}`)
-})
+  console.log(`App is running on http://localhost:${port}`);
+});
